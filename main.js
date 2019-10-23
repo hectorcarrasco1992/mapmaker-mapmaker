d931fc5e7277727c0233fc67a2e294bd916d5200
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-10-22 20:09:41 -0400

    can get 1 next to postive numbers  but not negative

--- main.js
+++ main.js
@@ -1,22 +1,90 @@
-function doubleAll() {
+function doubleAll(arr) {
+  let newArr = []
+  for(i = 0;i < arr.length; i++){
+    newArr.push(arr[i] * 2)
+  }
+  return newArr
 }
 
-function absoluteValues() {
+function absoluteValues(arr) {
+  let newArr = []
+  for (i = 0; i < arr.length; i++){
+    if ( arr[i] >= 0){
+      newArr.push(arr[i])
+    } else {
+      newArr.push(arr[i] * -1)
+    }
+
+    
+    
+  
+   }
+
+  return newArr
 }
 
-function yelledGreetings() {
+function yelledGreetings(arr) {
+  let newArr = []
+  
+  for (let i = 0; i < arr.length; i++){
+    newArr.push(arr[i]+ '!')
+
+  }
+  return newArr
 }
 
-function changeToInitials() {
+  
+  
+
+function changeToInitials(arr) {
+  let newArr = []
+  for (i = 0;i < arr.length;i++) {
+    for(j = 0;j < arr[i].length;j++){
+    if(j === 0){
+      newArr.push(arr[i][j])
+    } else if (arr[i][j] === ' ') {
+      newArr.push(arr[i][j+1])
+    }
+  }
+  }
+  return newArr
 }
 
-function doubleOdd() {
+function doubleOdd(arr) {
+  let newArr = []
+  for (let i = 0; i < arr.length; i++){
+  if(arr[i] % 2 === 1 || arr[i] % 2 === -1){
+    newArr.push(arr[i] * 2)
+  }else {
+    newArr.push(arr[i])
+  } 
+}
+   return newArr
 }
 
-function upperCaseFirstLetters() {
+function upperCaseFirstLetters(arr) {
+  let newArr = []
+   for(let i = 0; i <arr.length; i++){
+  //   if ( i <= 0){
+  //     newArr.push(arr[0].toUpperCase())
+  //   }
+  newArr.push(arr[i] + arr[0])
+  
+}
+return newArr
+
+ 
 }
 
-function add1ToLeft() {
+function add1ToLeft(arr) {
+  let left = []
+  let num = '1'
+  for (let i = 0; i < arr.length; i++){
+    if(arr[i] > 0|| arr[i] < 0){
+      left.push(num + arr[i])
+    }
+  }
+return left
 }
 
 
